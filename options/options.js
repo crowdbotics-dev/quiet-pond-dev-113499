9359eeb6a5622de977efbabe2d8004911564d08f
Crowdbotics <engineering@crowdbotics.com>
2023-09-01 15:05:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-pond-dev-113499",
+  url: "https://quiet-pond-dev-113499.botics.co",
+  api: "https://quiet-pond-dev-113499.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
